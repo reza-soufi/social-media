2bd8f6c60c73137f17dc57bf0a5682ee30dc217c
Reza Soufi <reza.soufi2014@gmail.com>
2023-09-10 09:34:14 +0330

    add initial project to git

---
+++ components/StyledComponents/MainContainer.tsx
@@ -0,0 +1,13 @@
+"use client";
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  max-width: 512px;
+  margin: 0 auto;
+  border: 1px solid;
+`;
